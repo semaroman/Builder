1651853b2856b9ce8315acb54a1dac4e749739b5
Роман Семашкевич <semaroman@mail.ru>
2023-11-01 18:24:25 +0300

    Commit

--- src/PersonBuilder.java
+++ src/PersonBuilder.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class PersonBuilder {
+public class PersonBuilder {
+    private String name;
+    private String surname;
+    private int age;
+    private String address;
+
+    public PersonBuilder setName(String name) throws IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Не хватает обязательных полей");
+        } else this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) throws IllegalArgumentException {
+        if (surname == null || surname.isEmpty()) {
+            throw new IllegalArgumentException("Не хватает обязательных полей");
+        } else this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) throws IllegalArgumentException {
+        if (age < 0) {
+            throw new IllegalArgumentException("Недопустимый возраст");
+        } else this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() throws IllegalStateException {
+        Person person;
+        if (name == null || surname == null)
+            throw new IllegalStateException("Не хватает обязательных полей");
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else person = new Person(name, surname, age);
+        person.setAddress(address);
+        return person;
+    }
 }
